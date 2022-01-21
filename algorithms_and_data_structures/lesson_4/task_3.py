7d7a4f3ad2b7adac25e0f305e31511daf6da0e8b
Eduard <evlisouski@gmail.com>
2022-01-21 15:38:23 +0300

    used cProfile library for testing

--- algorithms_and_data_structures/lesson_4/task_3.py
+++ algorithms_and_data_structures/lesson_4/task_3.py
@@ -1,3 +1,5 @@
+import cProfile
+
 
 def test_fib(func):
     lst = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
@@ -11,10 +13,14 @@ def fib(n):
         return n
     return fib(n - 1) + fib(n - 2)
 
+
+cProfile.run("fib(15)")
+# 1976 function calls (4 primitive calls) in 0.001 seconds
+# ncalls  tottime  percall  cumtime  percall filename:lineno(function
+# 1973/1    0.000    0.000    0.000    0.000 task_3.py:10(fib)
+
+
 # test_fib(fib)
 # 1000 loops, best of 5: 15.5 usec per loop     "task_3.fib(10)"
 # 1000 loops, best of 5: 173 usec per loop      "task_3.fib(15)"
 # 1000 loops, best of 5: 1.92 msec per loop     "task_3.fib(20)"
-
-
-
