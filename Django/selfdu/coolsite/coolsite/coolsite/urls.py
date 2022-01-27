04c48ca4a5a3d50fdf5b8c2e67f1664895e57ccc
Eduard <evlisouski@gmail.com>
2022-01-27 22:41:05 +0300

    feat: added handler404 for pageNotfound

--- Django/selfdu/coolsite/coolsite/coolsite/urls.py
+++ Django/selfdu/coolsite/coolsite/coolsite/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from women.views import pageNotFound
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('women.urls')),
 ]
+
+# Указано какую функцию вызывать при исключении 404
+handler404 = pageNotFound
