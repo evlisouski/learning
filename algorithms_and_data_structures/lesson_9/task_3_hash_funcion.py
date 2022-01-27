66d7f0cf7b532aa3dc93bd7df2b1a22590c6034d
Eduard <evlisouski@gmail.com>
2022-01-27 18:11:19 +0300

    init: start lesson

---
+++ algorithms_and_data_structures/lesson_9/task_3_hash_funcion.py
@@ -0,0 +1,41 @@
+# пример хеш словаря для английского алфавита размеров в 26 символов
+h_list = [None] * 26
+
+
+def my_append(value):
+    index = ord(value[0]) - ord('a')
+    h_list[index] = value
+    print(h_list)
+
+
+a = 'apriocot'
+my_append(a)
+
+b = 'banana'
+my_append(b)
+
+# 'apple' становится в ячейку с 'apriocot', пример колизии
+c = 'apple'
+my_append(c)
+
+# пример позиционной системы счисления, но схожей система можно получить хеш
+print(4625 == 4 * 10 ** 3 + 6 * 10 ** 2 + 2 * 10 ** 1 + 5 * 10 ** 0)
+
+
+
+# пример расчета хеша для слова с использованием следующей хеш функции
+# word_hash += (ord(char) - ord('a') + 1) * letter ** i
+def my_index(value):
+    letter = 26
+    word_hash = 0
+
+    # i - индекс буквы, char - текущий символ
+    for i, char in enumerate(value):
+        # для каждого символа высчитываем хеш и приболяем у предыдущему значению. В итоге получаем хеш сумму символов
+        word_hash += (ord(char) - ord('a') + 1) * letter ** i
+    return word_hash
+
+
+print(my_index(a))
+print(my_index(b))
+print(my_index(c))
