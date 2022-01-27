b841e3435000d04efe0b620b76063dba2faebe62
Eduard <evlisouski@gmail.com>
2022-01-27 22:42:32 +0300

    feat: created 3 view func

--- Django/selfdu/coolsite/coolsite/women/views.py
+++ Django/selfdu/coolsite/coolsite/women/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
-# Create your views here.
+def index(response):
+    return HttpResponse('Страница приложения women.')
+
+def categories(request, catid):
+    return HttpResponse(f"<h1> Статьи по категориям {catid}")
+
+def pageNotFound(request, exeption):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
