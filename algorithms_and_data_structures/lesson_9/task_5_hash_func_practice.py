7249d799e298e93e91b0bc6040640406ba6ba325
Eduard <evlisouski@gmail.com>
2022-01-28 13:59:12 +0300

    feat: added features for hash view

--- algorithms_and_data_structures/lesson_9/task_5_hash_func_practice.py
+++ algorithms_and_data_structures/lesson_9/task_5_hash_func_practice.py
@@ -2,7 +2,7 @@ import hashlib
 
 
 # функция сравнивает строки по хэш сумме и возвращает True если они равны
-def is_eq_str(a: str, b: str) -> bool:
+def is_eq_str(a: str, b: str, verbose=False) -> bool:
     assert len(a) > 0 and len(b) > 0, "Строки не могут быть пустые"
 
     # переводим строку из переменноа а и переводим в utf-8, получаем байт-строку
@@ -11,10 +11,13 @@ def is_eq_str(a: str, b: str) -> bool:
     # для строки b все то же самое
     hb = hashlib.sha1(b.encode('utf-8')).hexdigest()
 
+    if verbose:
+        print(f"hash 1 = {ha}\nhash 2 = {hb}")
+
     return ha == hb
 
 
 s_1 = input("Введите строку 1: ")
 s_2 = input("Введите строку 2: ")
 
-print("Строки одинаковые" if is_eq_str(s_1, s_2) else 'Строки разные')
+print("Строки одинаковые" if is_eq_str(s_1, s_2, True) else 'Строки разные')
