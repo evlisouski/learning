f2d6282f8085946d52219ca26b6619f5971c9c5a
Eduard <evlisouski@gmail.com>
2022-01-31 21:45:38 +0300

    feat: realized a simple frequency analysis

--- NLP/NLTK/ex_1.py
+++ NLP/NLTK/ex_1.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import nltk
 
 # region ВЫЗВАТЬ GUI для загрузки модулей и библиотек
@@ -12,4 +13,15 @@ if is_handle == "Y":
 # endregion
 
 
-# text_ = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+
+from nltk.tokenize import word_tokenize
+
+word_tokenized = word_tokenize(text)
+
+from nltk.probability import FreqDist
+
+fd = FreqDist(word_tokenized)
+print(fd.most_common(3))
+fd.plot(30, cumulative=False)
+plt.show()
