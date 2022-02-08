0075fe1bd3a19b7adfbd7551ff60e0337fa0cd03
Eduard <evlisouski@gmail.com>
2022-02-08 23:25:31 +0300

    feat: created contact form and integrated captcha APP

--- Django/selfdu/coolsite/coolsite/settings.py
+++ Django/selfdu/coolsite/coolsite/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'captcha',
     'debug_toolbar',
     'women.apps.WomenConfig',
 
--- Django/selfdu/coolsite/women/views.py
+++ Django/selfdu/coolsite/women/views.py
@@ -6,7 +6,7 @@ from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DeleteView, CreateView
+from django.views.generic import ListView, DeleteView, CreateView, FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import *
@@ -108,8 +108,22 @@ class AddPage(LoginRequiredMixin, DataMixin, CreateView):
         return context
 
 
-def contact(request):
-    return HttpResponse("Обратная связь")
+# def contact(request):
+#     return HttpResponse("Обратная связь")
+
+class ContactFormView(DataMixin, FormView):
+    form_class = ContactForm
+    template_name = 'women/contact.html'
+    success_url = reverse_lazy('home')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Обратная связь')
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return redirect('home')
 
 
 # def login(request):
