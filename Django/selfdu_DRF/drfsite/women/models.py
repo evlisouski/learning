eebbdaf798df38bc3c3e6f964b560aff551a2535
Eduard <evlisouski@gmail.com>
2022-02-18 23:18:23 +0300

    init: created app women and set up models

---
+++ Django/selfdu_DRF/drfsite/women/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class Women(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
+    content = models.TextField(blank=True, verbose_name='Текс статьи')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
+    is_published = models.BooleanField(default=True, verbose_name="Публикация")
+    # внешний ключ для связи один к многим
+    # ссылка в виде str 'Category' передается поскольку модель Category определяется после Women
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name="Категория")
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True, verbose_name="Категория")
+
+# вернуть имя катерогии
+    def __str__(self):
+        return self.name
