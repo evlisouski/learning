fea6bdb6778b871a36afa39a93356e22153587fb
Eduard <evlisouski@gmail.com>
2022-02-18 23:53:41 +0300

    feat: implemented primal API requests without serializer

--- Django/selfdu_DRF/drfsite/women/views.py
+++ Django/selfdu_DRF/drfsite/women/views.py
@@ -1,12 +1,20 @@
 from rest_framework import generics
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from women.models import Women
 from women.serializers import WomenSerializer
 
 
-class WomenAPIView(generics.ListAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
+class WomenAPIView(APIView):
+    def get(self, request):
+        return Response({'title': "Angelina Jolie"})
+
+
+
+# class WomenAPIView(generics.ListAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
 
 
