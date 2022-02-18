e9c3769c1b1b72b8fe5725abc1dddda4750ba0d1
Eduard <evlisouski@gmail.com>
2022-02-18 23:28:24 +0300

    feat: created superuser and registered app Women

--- Django/selfdu_DRF/drfsite/women/models.py
+++ Django/selfdu_DRF/drfsite/women/models.py
@@ -17,6 +17,7 @@ class Women(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True, verbose_name="Категория")
 
+
 # вернуть имя катерогии
     def __str__(self):
         return self.name
