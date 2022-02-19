08e4f195428c8c36bdf35bd607f37e8a64788ff4
Eduard <evlisouski@gmail.com>
2022-02-19 12:50:23 +0300

    feat: implemented serializators post/get method with model

--- Django/selfdu_DRF/drfsite/women/serializers.py
+++ Django/selfdu_DRF/drfsite/women/serializers.py
@@ -1,40 +1,45 @@
-import io
+# import io
 
 from rest_framework import serializers
-from rest_framework.parsers import JSONParser
-from rest_framework.renderers import JSONRenderer
-
-from women.models import Women
 
 
-class WomenModel:
-    def __init__(self, title, content):
-        self.title = title
-        self.content = content
+# from rest_framework.parsers import JSONParser
+# from rest_framework.renderers import JSONRenderer
 
-    # серриализатор
+
+# class WomenModel:
+#     def __init__(self, title, content):
+#         self.title = title
+#         self.content = content
+
+# сериализатор
+
 class WomenSerializer(serializers.Serializer):
-    # имена атрибутов должны быть одинаковые с моделью
     title = serializers.CharField(max_length=255)
     content = serializers.CharField()
+    # read_only = True указывает что поля не обязательны для записи
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    cat_id = serializers.IntegerField()
 
-# преобразование в модели в JSON строку
-def encode():
-    model = WomenModel('Angelina Jolie', "content: Angelina Jolie")
-    # создаем объект серриализатора, который создает специальную коллекцию data из атрибутов модели
-    # и представляет в виде словаря
-    model_sr = WomenSerializer(model)
-    print(model_sr.data, type(model_sr.data), sep='\n')
-    # JSONRenderer() преобразует объект серриализации в байтовую строку
-    json = JSONRenderer().render(model_sr.data)
-    print(json)
-
-def decode():
-    # имитация поступления запроса от клиента
-    stream = io.BytesIO(b'{"title":"Angelina Jolie","content":"content: Angelina Jolie"}')
-    # используем парсер
-    data = JSONParser().parse(stream)
-    # преобразовываем data c помощью объекта сериализации
-    serializer = WomenSerializer(data=data)
-    # делаем проверку декодированных данных на корректность
-    print(serializer.validated_data)
+# # преобразование в модели в JSON строку
+# def encode():
+#     model = WomenModel('Angelina Jolie', "content: Angelina Jolie")
+#     # создаем объект сериализатора, который создает специальную коллекцию data из атрибутов модели
+#     # и представляет в виде словаря
+#     model_sr = WomenSerializer(model)
+#     print(model_sr.data, type(model_sr.data), sep='\n')
+#     # JSONRenderer() преобразует объект сериализации в байтовую строку
+#     json = JSONRenderer().render(model_sr.data)
+#     print(json)
+# 
+# def decode():
+#     # имитация поступления запроса от клиента
+#     stream = io.BytesIO(b'{"title":"Angelina Jolie","content":"content: Angelina Jolie"}')
+#     # используем парсер
+#     data = JSONParser().parse(stream)
+#     # преобразовываем data c помощью объекта сериализации
+#     serializer = WomenSerializer(data=data)
+#     # делаем проверку декодированных данных на корректность
+#     print(serializer.validated_data)
--- Django/selfdu_DRF/drfsite/women/views.py
+++ Django/selfdu_DRF/drfsite/women/views.py
@@ -10,16 +10,23 @@ from women.serializers import WomenSerializer
 
 class WomenAPIView(APIView):
     def get(self, request):
-        lst = Women.objects.all().values()
-        return Response({'posts': list(lst)})
+        w = Women.objects.all()
+        # преобразование и передача в виде байтовой JSON строки
+        return Response({'posts': WomenSerializer(w, many=True).data})
 
     def post(self, request):
+        # создаём сериализатор на основании данных от POST запроса и распаковываем эти данные
+        serializer = WomenSerializer(data=request.data)
+        # проверяем корректность принятых данных
+        serializer.is_valid(raise_exception=True)
+
+
         post_new = Women.objects.create(
             title=request.data['title'],
             content=request.data['content'],
             cat_id=request.data['cat_id']
         )
-        return Response({"post": model_to_dict(post_new)})
+        return Response({"post": WomenSerializer(post_new).data})
 
 
 
