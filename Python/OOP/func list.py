76467690526154d3d6ea9dbd776621a5907ebc8b
Eduard <evlisouski@gmail.com>
2022-05-13 14:07:28 +0300

    feat: added materials from lesson 5

--- Python/OOP/func list.py
+++ Python/OOP/func list.py
@@ -1,3 +1,25 @@
+class A:
+    MIN_COORD = 0   #Атрибут MIN_COORD класса A c аргументом 0
+    MAX_COORD = 100
+
+    def __new__(cls, *args, **kwargs):  # вызывается перед созданием объекта класса
+        return None
+
+    def __init__(self, x=1, y=0):  # инициализатор класса
+        return None
+
+    def __del__(self):  # финализатор класса
+        return None
+
+
+    @classmethod    #Метод класса
+    def validate(cls, arg):
+        return cls.MIN_COORD <= arg <= cls.MAX_COORD
+
+    @staticmethod   #статический метод внутри класса
+    def norm2(x, y):
+        return x*x + y*y
+
 A.__doc__ #документация класса
 A.__dict__ #вернуть колекцию атрибутов текущего класса
 getattr(A, "attr1") #Вернуть значние указанного атрибута
@@ -5,11 +27,3 @@ hasattr(A, "attr1") #Проверить, есть ли указанный атр
 del A.attr1 #Удалить указанный атрибут
 setattr(A, "attr1") #Добавить/изменить указанный атрибут класса
 delattr(A, "attr1") #Удалить указанный атрибут
-
-
-def __init__(self, x=1, y=0):  # инициализатор класса
-def __del__(self):  # финализатор класса
-
-def __new__(cls, *args, **kwargs): #вызывается перед созданием объекта класса
-
-
