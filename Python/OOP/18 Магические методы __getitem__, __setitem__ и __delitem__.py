8181c0a3e3507a91958152535bed1c84ac372013
Eduard <evlisouski@gmail.com>
2022-05-16 19:04:08 +0300

    init: init

--- Python/OOP/18 Магические методы __getitem__, __setitem__ и __delitem__.py
+++ Python/OOP/18 Магические методы __getitem__, __setitem__ и __delitem__.py
@@ -3,5 +3,27 @@ class Student:
         self.name = name
         self.marks = list(marks)
 
-        
-    def __getitem__(self, item):
+    def __getitem__(self, item):  # получение значения по ключу item
+        if 0 <= item < len(self.marks):
+            return self.marks[item]
+        else:
+            raise IndexError("Неверный индекс")
+
+    def __setitem__(self, key, value):  # запись значения value по ключу key
+        if not isinstance(key, int) or key < 0:
+            raise TypeError("Индекс должен быть целым неотрицательным числом")
+        if key >= len(self.marks):
+            off = key + 1 - len(self.marks)
+            self.marks.extend([None] * off)
+        self.marks[key] = value
+
+    def __delitem__(self, key):  # удаление элемента по ключу key
+        if not isinstance(key, int):
+            raise TypeError('Индекс должен быть целым неотрицательным числом')
+        del self.marks[key]
+
+
+s1 = Student("Eduard", [5, 6, 7, 9, 2])
+print(s1[2])
+s1[2] = 4
+print(s1[2])
