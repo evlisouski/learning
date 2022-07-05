df0bc1014e28bb1c9a6372a7417aaefe0b8ef00c
evlisouski <evlisouski@gmail.com>
2022-07-05 22:25:34 +0300

    refactoring

--- Testing/TDD/superlists/functional_tests.py
+++ Testing/TDD/superlists/functional_tests.py
@@ -8,13 +8,14 @@ class NewVisitorTest(unittest.TestCase):
     def setUp(self) -> None:
         self.browser = webdriver.Firefox()
 
+
     def tearDown(self) -> None:
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get("http://localhost:8000")
         self.assertIn("To-Do", self.browser.title)
-        header_text = self.browser.find_element_by_name("username").text
+        header_text = self.browser.find_element("name", "h1").text
         self.assertIn("To-Do", header_text)
 
         inputbox = self.browser.find_element_by_id("id_new_item")
