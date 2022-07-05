6443125aff4d79433de6c2a9b23a61055e73f80a
evlisouski <evlisouski@gmail.com>
2022-07-05 23:39:44 +0300

    Now HTML generate with template

--- Testing/TDD/superlists/functional_tests.py
+++ Testing/TDD/superlists/functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 import unittest
@@ -13,12 +14,12 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get("http://localhost:8000")
+        self.browser.get("http://localhost:8001")
         self.assertIn("To-Do", self.browser.title)
-        header_text = self.browser.find_element("name", "h1").text
+        header_text = self.browser.find_element(By.TAG_NAME, "h1").text
         self.assertIn("To-Do", header_text)
 
-        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
         self.assertEqual(
             inputbox.get_attribute("placeholder"),
             "Enter a to-do item"
@@ -27,10 +28,11 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_element_by_tag_name("tr")
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_element(By.TAG_NAME, "tr")
         self.assertTrue(
-            any(row.text == "1: КУпить павлиньи перья" for row in rows)
+            any(row.text == "1: Купить павлиньи перья" for row in rows),
+            "новый элемент списка не появился в таблицу",
         )
 
         self.fail("Закончить тест!")
