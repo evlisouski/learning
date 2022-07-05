1a186a92feb348eb2877b3c44e2d6f2e4d7d4521
userx <evlisouski@gmail.com>
2022-07-05 16:06:59 +0300

    fix problem

--- Testing/TDD/superlists/superlists/urls.py
+++ Testing/TDD/superlists/superlists/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from lists import views
 
 urlpatterns = [
-    path(r'^$', views.home_page),
+    re_path(r'^$', views.home_page, name="home"),
     path('admin/', admin.site.urls),
 ]
