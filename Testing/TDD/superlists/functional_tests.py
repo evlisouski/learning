d8f2d6b3e0cfaaecd0d199b7f0e92bd4c6fa991c
userx <evlisouski@gmail.com>
2022-07-05 16:50:16 +0300

    save

--- Testing/TDD/superlists/functional_tests.py
+++ Testing/TDD/superlists/functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -12,6 +14,24 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get("http://localhost:8000")
         self.assertIn("To-Do", self.browser.title)
+        header_text = self.browser.find_element_by_name("username").text
+        self.assertIn("To-Do", header_text)
+
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertEqual(
+            inputbox.get_attribute("placeholder"),
+            "Enter a to-do item"
+        )
+
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_element_by_tag_name("tr")
+        self.assertTrue(
+            any(row.text == "1: КУпить павлиньи перья" for row in rows)
+        )
+
         self.fail("Закончить тест!")
 
 
