48aa5e1753a1d477bde35aac3eba807b574b486e
evlisouski <evlisouski@gmail.com>
2022-07-06 18:20:07 +0300

    ref: refactored func. tests

--- Testing/TDD/superlists/functional_tests.py
+++ Testing/TDD/superlists/functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self) -> None:
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get("http://localhost:8001")
         self.assertIn("To-Do", self.browser.title)
@@ -25,30 +30,18 @@ class NewVisitorTest(unittest.TestCase):
         )
 
         inputbox.send_keys("Купить павлиньи перья")
-
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")
-
-        self.assertTrue(
-            any(row.text == "1: Купить павлиньи перья" for row in rows),
-            f"новый элемент списка не появился в таблице. Содержимое было равно: n{table.text}",
-        )
-
+        self.check_for_row_in_list_table("1: Купить павлиньи перья")
         inputbox = self.browser.find_element(By.ID, "id_new_item")
         inputbox.send_keys("Сделать мушку из павлиньих перьев")
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table("1: Купить павлиньи перья")
+        self.check_for_row_in_list_table("2: Сделать мушку из павлиньих перьев")
 
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")
 
-        self.assertIn("1: Купить павлиньи перья", [row.text for row in rows])
-        self.assertIn(
-            "2: Сделать мушку из павлиньих перьев", [row.text for row in rows]
-        )
+
 
 
 
